3b70514c1c75307030c5e77928d5343174cab1e6
Nick <nikolayshilov@yahoo.com>
2022-01-10 14:45:04 -0500

    work 3 is done

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,10 @@ public class HelloWorld {
 /*
 Doing some work here
  */
-    //Doing awesome work 2
+    //Doing awesome work 2!
+
+    //Doing awesome work 3!
+    //Work 3 is ready to be committed and pushed to remote repository
+    //Doing awesome work 4!
 
 }
